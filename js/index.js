77f9bd224816f0c7256bd022619067da252a4b10
Ai Jian <53556553+ai-jian@users.noreply.github.com>
2022-04-12 23:48:09 -0500

    add custom cursor

--- js/index.js
+++ js/index.js
@@ -28,73 +28,30 @@ ScrollTrigger.create({
 });
 
 
-// // custom cursor
-// const cursor = document.querySelector('#cursor');
-// const cursorCircle = cursor.querySelector('.cursor__circle');
+//custom cursor
+const cursor = document.querySelector(".app__cursor")
+const follower = document.querySelector(".cursor-follower")
+const cursorScale = document.querySelectorAll(".app__cursor-scale")
 
-// const mouse = { x: -100, y: -100 }; // mouse pointer's coordinates
-// const pos = { x: 0, y: 0 }; // cursor's coordinates
-// const speed = 0.1; // between 0 and 1
+gsap.set('.app__cursor', { xPercent: -50, yPercent: -50 })
+gsap.set('.cursor-follower', { xPercent: -50, yPercent: -50 })
 
-// const updateCoordinates = e => {
-//     mouse.x = e.clientX;
-//     mouse.y = e.clientY;
-// }
+window.addEventListener('mousemove', (e) => {
+    gsap.to(cursor, 0.2, { x: e.clientX, y: e.clientY })
+    gsap.to(follower, 0.9, { x: e.clientX, y: e.clientY })
+})
 
-// window.addEventListener('mousemove', updateCoordinates);
+// cursorScale.classList.add('cursor-grow')
 
+cursorScale.forEach(link => {
+    link.addEventListener('mousemove', () => {
+        link.classList.contains('cursor-small') ?
+            cursor.classList.add('grow-small') :
+            cursor.classList.add('cursor-grow')
 
-// function getAngle(diffX, diffY) {
-//     return Math.atan2(diffY, diffX) * 180 / Math.PI;
-// }
-
-// function getSqueeze(diffX, diffY) {
-//     const distance = Math.sqrt(
-//         Math.pow(diffX, 2) + Math.pow(diffY, 2)
-//     );
-//     const maxSqueeze = 0.15;
-//     const accelerator = 1500;
-//     return Math.min(distance / accelerator, maxSqueeze);
-// }
-
-
-// const updateCursor = () => {
-//     const diffX = Math.round(mouse.x - pos.x);
-//     const diffY = Math.round(mouse.y - pos.y);
-
-//     pos.x += diffX * speed;
-//     pos.y += diffY * speed;
-
-//     const angle = getAngle(diffX, diffY);
-//     const squeeze = getSqueeze(diffX, diffY);
-
-//     const scale = 'scale(' + (1 + squeeze) + ', ' + (1 - squeeze) + ')';
-//     const rotate = 'rotate(' + angle + 'deg)';
-//     const translate = 'translate3d(' + pos.x + 'px ,' + pos.y + 'px, 0)';
-
-//     cursor.style.transform = translate;
-//     cursorCircle.style.transform = rotate + scale;
-// };
-
-// function loop() {
-//     updateCursor();
-//     requestAnimationFrame(loop);
-// }
-
-// requestAnimationFrame(loop);
-
-
-
-// const cursorModifiers = document.querySelectorAll('[cursor-class]');
-
-// cursorModifiers.forEach(curosrModifier => {
-//     curosrModifier.addEventListener('mouseenter', function () {
-//         const className = this.getAttribute('cursor-class');
-//         cursor.classList.add(className);
-//     });
-
-//     curosrModifier.addEventListener('mouseleave', function () {
-//         const className = this.getAttribute('cursor-class');
-//         cursor.classList.remove(className);
-//     });
-// });
+    })
+    link.addEventListener('mouseleave', () => {
+        cursor.classList.remove('cursor-grow')
+        cursor.classList.remove('grow-small')
+    })
+})
